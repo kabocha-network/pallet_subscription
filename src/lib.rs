1f5ac65e37801450a99f000f813a43291275c059
nathan gd <nathan.gardet-derc@pm.me>
2022-06-14 16:41:02 +0200

    updated with what we'll show

--- src/lib.rs
+++ src/lib.rs
@@ -28,9 +28,9 @@ pub mod pallet {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 		/// The trait to manage funds
 		type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
-        /// The maximum amount of metadata
-        #[pallet::constant]
-        type MaxMetadataLength: Get<u32>;
+		/// The maximum amount of metadata
+		#[pallet::constant]
+		type MaxMetadataLength: Get<u32>;
 	}
 
 	#[pallet::pallet]
@@ -38,66 +38,21 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	#[pallet::storage]
-	#[pallet::getter(fn subscription_plans)]
-	pub type SubscriptionPlans<T: Config> =
-		StorageMap<_, Blake2_256, Nonce, SubscriptionPlan<T>, OptionQuery>;
+	#[pallet::getter(fn plan_nonce)]
+	pub type PlanNonce<T> = StorageValue<_, PlanId, ValueQuery>;
 
 	#[pallet::storage]
-	#[pallet::getter(fn plan_nonce)]
-	pub type PlanNonce<T> = StorageValue<_, Nonce, ValueQuery>;
+	#[pallet::getter(fn subscription_plans)]
+	pub type Plans<T: Config> = StorageMap<_, Blake2_256, PlanId, Subscription<T>, OptionQuery>;
 
-    // actually the best solution rn because we dont need that much key ? idk
+	#[pallet::storage]
+	#[pallet::getter(fn subscription_nonce)]
+	pub type SubscriptionNonce<T> = StorageValue<_, SubscriptionId, ValueQuery>;
+
 	#[pallet::storage]
 	#[pallet::getter(fn subscriptions)]
-    pub type Subscriptions<T: Config> =
-        StorageMap<_, Blake2_256, Nonce, Subscription<T>, OptionQuery>;
-
-
-    //// OPTION WITH A LOT OF STORAGE
-    /// way better for the execution, but some constraints
-    ///
-    /// - check if a subscription is running = 4 reads, not cool // oooor maybe 2 read, get the
-    /// recurence in the plans, and get in the right storage ?
-    /// - we can have ONLY 4 types of recurence (or we need another storage for each recurence)
-
-    // pub type DailySubscriptions<T: Config> =
-    //     StorageMap<_, Blake2_256, Nonce, Subscription<T>, OptionQuery>;
-    // pub type WeeklySubscriptions<T: Config> =
-    //     StorageMap<_, Blake2_256, Nonce, Subscription<T>, OptionQuery>;
-    // pub type MonthlySubscriptions<T: Config> =
-    //     StorageMap<_, Blake2_256, Nonce, Subscription<T>, OptionQuery>;
-    // pub type AnnualySubscriptions<T: Config> =
-    //     StorageMap<_, Blake2_256, Nonce, Subscription<T>, OptionQuery>;
-    //
-
-	// might be a good way too but more complicated idk
-    // first solution simpler atm
-    //
-	// #[pallet::storage]
-	// #[pallet::getter(fn subscriptions)]
- //    pub type Subscriptions<T: Config> =
- //        StorageDoubleMap<
- //        _,
- //        Blake2_256,
- //        AccountId,
- //        Blake2_256,
- //        Nonce,
- //        Subscription<T>,
- //        OptionQuery,
- //    >;
- //
- //    easier for execution ?
- //
- //    pub type Subscriptions<T: Config> =
- //        StorageDoubleMap<
- //        _,
- //        Blake2_256,
- //        AccountId,
- //        Blake2_256,
- //        Nonce,
- //        Recurence, // or block_number or idk
- //        OptionQuery,
- //    >;
+	pub type Subscriptions<T: Config> =
+		StorageMap<_, Blake2_256, SubscriptionId, Subscription<T>, OptionQuery>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -110,83 +65,79 @@ pub mod pallet {
 		NoneValue,
 	}
 
-    // this example comes from treasury
-    //
-	// #[pallet::hooks]
-	// impl<T: Config<I>, I: 'static> Hooks<BlockNumberFor<T>> for Pallet<T, I> {
-	// 	/// # <weight>
-	// 	/// - Complexity: `O(A)` where `A` is the number of approvals
-	// 	/// - Db reads and writes: `Approvals`, `pot account data`
-	// 	/// - Db reads and writes per approval: `Proposals`, `proposer account data`, `beneficiary
-	// 	///   account data`
-	// 	/// - The weight is overestimated if some approvals got missed.
-	// 	/// # </weight>
-	// 	fn on_initialize(n: T::BlockNumber) -> Weight {
-	// 		// Check to see if we should spend some funds!
-	// 		if (n % T::SpendPeriod::get()).is_zero() {
-	// 			Self::spend_funds()
-	// 		} else {
-	// 			0
-	// 		}
-	// 	}
-	// }
-    //
-    // this is OUR hook
-    // #[pallet::hooks]
-    // impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
-	//     fn on_initialize(n: T::BlockNumber) -> Weight {
-    //         //// prop 1:
-    //         - loop over all subscriptions (.iter_values())
-    //         - check if the subscription shoud be taken
-    //         - take it
-    //
-    //         //// prop 2: (2nd key: )
-    //
-    // 	   }
-    // }
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_initialize(n: T::BlockNumber) -> Weight {
+			//// prop 1:
+			// - loop over all subscriptions (.iter_values())
+			// - check if the subscription shoud be taken
+			// - take it
+
+			//// prop 2: (2nd key: )
+			0
+		}
+	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::weight(1)]
-		pub fn create_subsciption_plan(origin: OriginFor<T>, plan: SubscriptionPlan<T>) -> DispatchResult {
-            // maybe make a new function for Subscription, so `plan` is already valid 100%
+		pub fn create_subsciption_plan(
+			origin: OriginFor<T>,
+			plan: Subscription<T>,
+		) -> DispatchResult {
+			// maybe make a new function for Subscription, so `plan` is already valid 100%
+			//
+			//
+			// todo:
+            // signed ?
+            // valid plan ?
             //
-            //
-            // todo:
-            // push the plan to the storage
-			Ok(())
-		}
-
-		#[pallet::weight(1)]
-		pub fn another_create_subsciption_plan(origin: OriginFor<T>, /* data of the struct so we can verify */) -> DispatchResult {
-            // better ? 5 parameters but we can check, better option if no plan.new() function
-            //
-            //
-            // todo:
-            // push the plan to the storage
+			// -> push the plan to the storage
 			Ok(())
 		}
 
 		#[pallet::weight(1)]
 		pub fn delete_subsciption_plan(origin: OriginFor<T>, plan_id: Nonce) -> DispatchResult {
-            // better ? 5 parameters but we can check, better option if no plan.new() function
+			// better ? 5 parameters but we can check, better option if no plan.new() function
+			//
+			//
+			// todo:
+            // signed ?
+            // plan exist ?
             //
-            //
-            // todo:
-            // push the plan to the storage
+			// -> remove the plan to the storage
 			Ok(())
 		}
 
-        #[pallet::weight(1)]
-        pub fn subscribe(origin: OriginFor<T>, plan_id: Nonce) -> DispatchResult {
-            // todo:
-            // check if plan exist
-            // if yes, push the nonce to the subscription storage
-        }
+		#[pallet::weight(1)]
+		pub fn subscribe_to_plan(origin: OriginFor<T>, plan_id: Nonce) -> DispatchResult {
+			// todo:
+			// signed ?
+            // plan exist ?
+            //
+			// -> create entry in storage
+			Ok(())
+		}
 
-        #[pallet::weight(1)]
-        pub fn unsubscribe(origin: OriginFor<T>, plan_id: Nonce) -> DispatchResult {
-            //kinda obv
-        }
+		#[pallet::weight(1)]
+		pub fn subscribe(origin: OriginFor<T>, plan: Subscription) -> DispatchResult {
+			// todo:
+			// signed ?
+            // valid subscription ?
+            //
+			// -> create entry in storage
+			Ok(())
+		}
+
+		#[pallet::weight(1)]
+		pub fn unsubscribe(origin: OriginFor<T>, other: T::AccountId) -> DispatchResult {
+			// signed by the subscriber ?
+            // yes -> subscriber is subscribed to other ?
+            //        delete from storage
+            // no -> signed by beneficiary ?
+            //       other is subscribed to beneficiary ?
+            //       delete from storage
+			Ok(())
+		}
 	}
 }
--- src/types.rs
+++ src/types.rs
@@ -1,29 +1,33 @@
 use crate::*;
 use scale_info::TypeInfo;
 use codec::{Decode, Encode};
-use frame_support::{
-    storage::bounded_vec::BoundedVec,
-    pallet_prelude::MaxEncodedLen,
-};
+// use frame_support::{
+//     // storage::bounded_vec::BoundedVec,
+//     pallet_prelude::MaxEncodedLen,
+// };
 
 pub type BalanceOf<T> =
 	<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
 pub type Nonce = u64;
+pub type PlanId = u64;
+pub type SubscriptionId = u64;
 
-#[derive(Clone, Encode, Decode, TypeInfo, Debug, MaxEncodedLen)]
-pub enum Frequency {
-    Daily,
-    Weekly,
-    Monthly,
-    Annualy,
-}
+// #[derive(Clone, Encode, Decode, TypeInfo, Debug, MaxEncodedLen)]
+// pub enum Frequency {
+//     Daily,
+//     Weekly,
+//     Monthly,
+//     Annualy,
+// }
 
-#[derive(Clone, Encode, Decode, TypeInfo, Debug, MaxEncodedLen)]
-#[codec(mel_bound())]
-pub struct SubscriptionPlan<T: Config> {
-    pub frequency: Frequency,
+#[derive(Clone, Encode, Decode, TypeInfo, Debug)]
+// #[codec(mel_bound())]
+pub struct Subscription<T: Config> {
+    pub frequency: u32, // block number
     pub amount: BalanceOf<T>,
+    pub remaining_payments: Option<u32>,
     pub beneficiary: T::AccountId,
-    pub metadata: BoundedVec<u8, T::MaxMetadataLength>
+    // for later
+    // pub metadata: BoundedVec<u8, T::MaxMetadataLength>
 }
