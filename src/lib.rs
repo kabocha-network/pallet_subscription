80d09bc814110f94e6944e4c746bf713c5798622
nathan gd <nathan.gardet-derc@pm.me>
2022-06-16 18:30:23 +0200

    added the hook

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,24 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+#![feature(drain_filter)]
+#![allow(clippy::type_complexity)]
 
 pub use pallet::*;
 
 pub mod types;
 pub use types::*;
 
-pub use frame_support::traits::tokens::currency::{Currency, ReservableCurrency};
+pub use frame_support::{
+	storage::IterableStorageMap,
+	traits::tokens::{
+		currency::{Currency, ReservableCurrency},
+		ExistenceRequirement,
+	},
+	ReversibleStorageHasher,
+};
 
-// #[cfg(test)]
-// mod mock;
-//
-// #[cfg(test)]
-// mod tests;
-//
+#[cfg(test)]
+mod tests;
+
 // #[cfg(feature = "runtime-benchmarks")]
 // mod benchmarking;
 
@@ -39,23 +45,39 @@ pub mod pallet {
 
 	#[pallet::storage]
 	#[pallet::getter(fn plan_nonce)]
-	pub type PlanNonce<T> = StorageValue<_, PlanId, ValueQuery>;
+	pub type PlanNonce<T: Config> = StorageValue<_, PlanId, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn subscription_plans)]
-	pub type Plans<T: Config> = StorageMap<_, Blake2_256, PlanId, Subscription<T>, OptionQuery>;
+	pub type Plans<T: Config> = StorageMap<
+		_,
+		Blake2_256,
+		PlanId,
+		Subscription<T::BlockNumber, BalanceOf<T>, T::AccountId>,
+		OptionQuery,
+	>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn subscription_nonce)]
-	pub type SubscriptionNonce<T> = StorageValue<_, SubscriptionId, ValueQuery>;
+	pub type SubscriptionNonce<T: Config> = StorageValue<_, SubscriptionId, ValueQuery>;
 
+	// the vector should ALWAYS be sorted from biggest to smallest block_number
 	#[pallet::storage]
+	#[pallet::unbounded]
 	#[pallet::getter(fn subscriptions)]
-	pub type Subscriptions<T: Config> =
-		StorageMap<_, Blake2_256, SubscriptionId, Subscription<T>, OptionQuery>;
+	pub type Subscriptions<T: Config> = StorageValue<
+		_,
+		Vec<(
+			T::BlockNumber,
+			(
+				Subscription<T::BlockNumber, BalanceOf<T>, T::AccountId>,
+				T::AccountId,
+			),
+		)>,
+	>;
 
 	#[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	// #[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		SomethingStored(u32, T::AccountId),
 	}
@@ -68,13 +90,45 @@ pub mod pallet {
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		fn on_initialize(n: T::BlockNumber) -> Weight {
-			//// prop 1:
-			// - loop over all subscriptions (.iter_values())
-			// - check if the subscription shoud be taken
-			// - take it
+			let _weight = 0;
 
-			//// prop 2: (2nd key: )
-			0
+			let mut subs = unwrap_or_return!(Subscriptions::<T>::get(), 0); // 1 read
+			let tmp = subs.clone();
+			let (exec_block, (_, _)) = unwrap_or_return!(tmp.last(), 0); // 1 unwrap
+			if n != *exec_block {
+				return 0
+			}
+			loop {
+				if let Some((b, (mut sub, account))) = subs.pop() {
+					if b == *exec_block {
+						if let Some(val) = sub.remaining_payments {
+							if val == 0 {
+								subs.pop();
+								continue
+							}
+						}
+						if T::Currency::transfer(
+							&account,
+							&sub.beneficiary,
+							sub.amount,
+							ExistenceRequirement::AllowDeath,
+						)
+						.is_err()
+						{
+							continue
+						}
+						sub.remaining_payments = sub.remaining_payments.map(|amount| amount - 1);
+						let new_block = n + sub.frequency;
+						let new_index =
+							subs.binary_search_by(|(b, _)| b.cmp(&new_block)).unwrap_or_else(|e| e);
+						subs.insert(new_index, (new_block, (sub.clone(), account.clone())));
+					}
+				}
+				if (unwrap_or_return!(subs.last(), 0)).0 == *exec_block {
+					Subscriptions::<T>::set(Some(subs));
+					return 0 // WEIGHT
+				}
+			}
 		}
 	}
 
@@ -82,61 +136,64 @@ pub mod pallet {
 	impl<T: Config> Pallet<T> {
 		#[pallet::weight(1)]
 		pub fn create_subsciption_plan(
-			origin: OriginFor<T>,
-			plan: Subscription<T>,
+			_origin: OriginFor<T>,
+			_plan: Subscription<T::BlockNumber, BalanceOf<T>, T::AccountId>,
 		) -> DispatchResult {
 			// maybe make a new function for Subscription, so `plan` is already valid 100%
 			//
 			//
 			// todo:
-            // signed ?
-            // valid plan ?
-            //
+			// signed ?
+			// valid plan ?
+			//
 			// -> push the plan to the storage
 			Ok(())
 		}
 
 		#[pallet::weight(1)]
-		pub fn delete_subsciption_plan(origin: OriginFor<T>, plan_id: Nonce) -> DispatchResult {
+		pub fn delete_subsciption_plan(_origin: OriginFor<T>, _plan_id: Nonce) -> DispatchResult {
 			// better ? 5 parameters but we can check, better option if no plan.new() function
 			//
 			//
 			// todo:
-            // signed ?
-            // plan exist ?
-            //
+			// signed ?
+			// plan exist ?
+			//
 			// -> remove the plan to the storage
 			Ok(())
 		}
 
 		#[pallet::weight(1)]
-		pub fn subscribe_to_plan(origin: OriginFor<T>, plan_id: Nonce) -> DispatchResult {
+		pub fn subscribe_to_plan(_origin: OriginFor<T>, _plan_id: Nonce) -> DispatchResult {
 			// todo:
 			// signed ?
-            // plan exist ?
-            //
+			// plan exist ?
+			//
 			// -> create entry in storage
 			Ok(())
 		}
 
 		#[pallet::weight(1)]
-		pub fn subscribe(origin: OriginFor<T>, plan: Subscription) -> DispatchResult {
+		pub fn subscribe(
+			_origin: OriginFor<T>,
+			_plan: Subscription<T::BlockNumber, BalanceOf<T>, T::AccountId>,
+		) -> DispatchResult {
 			// todo:
 			// signed ?
-            // valid subscription ?
-            //
+			// valid subscription ?
+			//
 			// -> create entry in storage
 			Ok(())
 		}
 
 		#[pallet::weight(1)]
-		pub fn unsubscribe(origin: OriginFor<T>, other: T::AccountId) -> DispatchResult {
+		pub fn unsubscribe(_origin: OriginFor<T>, _other: T::AccountId) -> DispatchResult {
 			// signed by the subscriber ?
-            // yes -> subscriber is subscribed to other ?
-            //        delete from storage
-            // no -> signed by beneficiary ?
-            //       other is subscribed to beneficiary ?
-            //       delete from storage
+			// yes -> subscriber is subscribed to other ?
+			//        delete from storage
+			// no -> signed by beneficiary ?
+			//       other is subscribed to beneficiary ?
+			//       delete from storage
 			Ok(())
 		}
 	}
---
+++ src/tests/hook.rs
@@ -0,0 +1,45 @@
+use super::mock::*;
+use frame_support::traits::OnInitialize;
+
+#[test]
+fn working_subscription() {
+	ExtBuilder::default().balances(vec![]).build().execute_with(|| {
+		let _x = 10;
+		let _bob_balance = Balances::free_balance(BOB());
+		let _alice_balance = Balances::free_balance(ALICE());
+
+		//TODO: make a subscription of x amount, that recur every 1 block, from ALICE() to BOB()
+
+		<Subscription as OnInitialize<u64>>::on_initialize(1);
+
+		//assert_eq!(Balances::free_balance(BOB()), bob_balance + _x);
+		//assert_eq!(Balances::free_balance(ALICE()), alice_balance - _x);
+		//TODO: check if the subscription is still running (should be OK)
+	})
+}
+
+#[test]
+fn sub_last_occurence() {
+	ExtBuilder::default().balances(vec![]).build().execute_with(|| {
+		let _x = 10;
+		let _bob_balance = Balances::free_balance(BOB());
+		let _alice_balance = Balances::free_balance(ALICE());
+
+		//TODO: make a subscription of x amount, that recur every 1 block, from ALICE() to BOB().
+		// with remaining_payments = 1, this should only be executed once.
+
+		<Subscription as OnInitialize<u64>>::on_initialize(1);
+
+		//assert_eq!(Balances::free_balance(BOB()), bob_balance + _x);
+		//assert_eq!(Balances::free_balance(ALICE()), alice_balance - _x);
+
+		//TODO: check if the subscription is still running (should be OK)
+
+		<Subscription as OnInitialize<u64>>::on_initialize(2);
+
+		//assert_eq!(Balances::free_balance(BOB()), bob_balance + _x);
+		//assert_eq!(Balances::free_balance(ALICE()), alice_balance - _x);
+
+		//TODO: check if the subscription is still running (should be KO)
+	})
+}
