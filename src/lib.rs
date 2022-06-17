e13a3b925be9c11f8fe8df1242cd6e8c3cfa58f1
nathan gd <nathan.gardet-derc@pm.me>
2022-06-17 16:19:32 +0200

    added weight structure for hook

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ pub use pallet::*;
 pub mod types;
 pub use types::*;
 
+pub mod weights;
+pub use weights::*;
+
 pub use frame_support::{
 	storage::IterableStorageMap,
 	traits::tokens::{
@@ -33,9 +36,9 @@ pub mod pallet {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 		/// The trait to manage funds
 		type Currency: Currency<Self::AccountId>;
-		/// The maximum amount of metadata
-		#[pallet::constant]
-		type MaxMetadataLength: Get<u32>;
+
+		/// Weigths module
+		type WeightInfo: WeightInfo;
 	}
 
 	#[pallet::pallet]
@@ -161,20 +164,23 @@ pub mod pallet {
 
 	impl<T: Config> Pallet<T> {
         pub fn do_execute_subscriptions(n: T::BlockNumber) -> Weight {
-            let mut subs = unwrap_or_return!(Subscriptions::<T>::get(), 0); // 1 read
+            let mut itterations = 0;
+
+            let mut subs = unwrap_or_return!(Subscriptions::<T>::get(), 0);
             let tmp = subs.clone();
-            let (exec_block, (_, _)) = unwrap_or_return!(tmp.last(), 0); // 1 unwrap
-            if n != *exec_block {
+            let (exec_block, (_, _)) = unwrap_or_return!(tmp.last(), 0);
+            if n > *exec_block {
                 return 0
             }
             loop {
-                if let Some((b, (mut sub, account))) = subs.pop() {
-                    if b == *exec_block {
+                if let Some((block, (mut sub, account))) = subs.pop() {
+                    if block <= *exec_block {
                         if let Some(val) = sub.remaining_payments {
                             if val == 0 {
                                 continue
                             }
                         }
+                        itterations += 1;
                         if T::Currency::transfer(
                             &account,
                             &sub.beneficiary,
@@ -186,17 +192,18 @@ pub mod pallet {
                             continue
                         }
                         sub.remaining_payments = sub.remaining_payments.map(|amount| amount - 1);
-                        let new_block = n + sub.frequency;
+                        let new_block = n + sub.frequency - (*exec_block - n);
                         let new_index =
-                            subs.binary_search_by(|(b, _)| b.cmp(&new_block)).unwrap_or_else(|e| e);
+                            subs.binary_search_by(|(block, _)| block.cmp(&new_block)).unwrap_or_else(|e| e);
                         subs.insert(new_index, (new_block, (sub.clone(), account.clone())));
                     }
                 }
                 if (unwrap_or_return!(subs.last(), 0)).0 == *exec_block {
                     Subscriptions::<T>::set(Some(subs));
-                    return 0
+                    break;
                 }
             }
+            T::WeightInfo::do_execute_subscriptions(itterations)
         }
     }
 }
---
+++ src/weights.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(rustfmt, rustfmt_skip)]
+#![allow(unused_parens)]
+#![allow(unused_imports)]
+
+use frame_support::{traits::Get, weights::Weight};
+use sp_std::marker::PhantomData;
+
+pub trait WeightInfo {
+    fn do_execute_subscriptions(itts: u32) -> Weight;
+}
+
+/// Weight functions for `pallet_supersig`.
+pub struct SubstrateWeight<T>(PhantomData<T>);
+impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
+	fn do_execute_subscriptions(itts: u32) -> Weight {
+		(0u64)
+			.saturating_add(T::DbWeight::get().reads(1u64))
+			.saturating_add(T::DbWeight::get().writes(1u64))
+			.saturating_add((0u64).saturating_mul(itts as Weight))
+	}
+}
