a17e4fbe88e28f15fe292e0463739a27634e683c
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-09-25 19:55:38 +0200

    test(on_initialize): add infinite and caped instalment tests

--- src/tests/on_initialize.rs
+++ src/tests/on_initialize.rs
@@ -3,7 +3,7 @@ use crate::{Error, Subscription};
 use frame_support::{assert_noop, assert_ok};
 
 #[test]
-fn trigger_hook_once() {
+fn trigger_hook_once_transfer_funds() {
 	ExternalityBuilder::default().build().execute_with(|| {
 		let amount = 4000;
 		let frequency = 5;
@@ -28,7 +28,38 @@ fn trigger_hook_once() {
 			alice_balance_before - amount
 		);
 		assert_eq!(Balances::free_balance(&BOB()), bob_balance_before + amount);
-		let subscriptions_to_come = PalletSubscription::subscriptions(2 + frequency);
+	})
+}
+
+#[test]
+fn infinite_subscription() {
+	ExternalityBuilder::default().build().execute_with(|| {
+		let amount = 4000;
+		let frequency = 5;
+		let beneficiary = BOB();
+		let recurence = None;
+
+		let alice_balance_before = Balances::free_balance(&ALICE());
+		let bob_balance_before = Balances::free_balance(&BOB());
+
+		assert_ok!(PalletSubscription::subscribe(
+			Origin::signed(ALICE()),
+			beneficiary.clone(),
+			amount,
+			frequency,
+			recurence,
+		));
+
+		run_to_block(2 + frequency * 100);
+		assert_eq!(
+			Balances::free_balance(&ALICE()),
+			alice_balance_before - amount * 101
+		);
+		assert_eq!(
+			Balances::free_balance(&BOB()),
+			bob_balance_before + amount * 101
+		);
+		let subscriptions_to_come = PalletSubscription::subscriptions(2 + frequency * 101);
 		assert!(
 			subscriptions_to_come
 				.expect("Should contain the reinserted subscription")
@@ -42,5 +73,59 @@ fn trigger_hook_once() {
 					ALICE()
 				))
 		)
+	})
+}
+
+#[test]
+fn caped_instalment_subscription() {
+	ExternalityBuilder::default().build().execute_with(|| {
+		let amount = 4000;
+		let frequency = 5;
+		let beneficiary = BOB();
+		let recurence = Some(10);
+
+		let alice_balance_before = Balances::free_balance(&ALICE());
+		let bob_balance_before = Balances::free_balance(&BOB());
+
+		assert_ok!(PalletSubscription::subscribe(
+			Origin::signed(ALICE()),
+			beneficiary.clone(),
+			amount,
+			frequency,
+			recurence,
+		));
+
+		run_to_block(2);
+		assert_eq!(
+			Balances::free_balance(&ALICE()),
+			alice_balance_before - amount
+		);
+		assert_eq!(Balances::free_balance(&BOB()), bob_balance_before + amount);
+		let subscriptions_to_come = PalletSubscription::subscriptions(2 + frequency);
+		assert!(
+			subscriptions_to_come
+				.expect("Should contain the reinserted subscription")
+				.contains(&(
+					Subscription {
+						frequency,
+						amount,
+						remaining_payments: Some(recurence.unwrap() - 1),
+						beneficiary,
+					},
+					ALICE()
+				))
+		);
+
+		run_to_block(2 + frequency * (recurence.unwrap() as u64 - 1));
+		assert_eq!(
+			Balances::free_balance(&ALICE()),
+			alice_balance_before - amount * 10
+		);
+		assert_eq!(
+			Balances::free_balance(&BOB()),
+			bob_balance_before + amount * 10
+		);
+		let subscriptions_to_come = PalletSubscription::subscriptions(2 + frequency * 10);
+		assert!(subscriptions_to_come.is_none())
 	})
 }
